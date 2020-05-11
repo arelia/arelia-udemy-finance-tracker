768be10c4b4a6b80a773d142c34bb503a1a16197
Arelia <arelia@github.com>
2020-05-11 17:03:01 -0500

    render friend search result partials

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -9,4 +9,27 @@ class FriendsController < ApplicationController
     render json: @friend
   end
 
+
+  def search
+    if params[:friends].present?
+      @friend = params[:friends]
+      # @friend = User.where(first_name: params[:friends])
+      if @friend
+        respond_to do |format|
+          format.js { render partial: 'friends/result' }
+        end
+      else
+        respond_to do |format|
+          flash.now[:alert] = "Couldn't find user"
+          format.js { render partial: 'friends/result' }
+        end
+      end    
+    else
+      respond_to do |format|
+        flash.now[:alert] = "Please enter a friend name or email to search"
+        format.js { render partial: 'friends/result' }
+      end
+    end
+  end
+
 end
